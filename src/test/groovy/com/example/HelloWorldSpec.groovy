d6e2afe8139f3e90aa14d8d275c3e0641e25fc43
oinsio <8233537+oinsio@users.noreply.github.com>
2026-08-14 21:53:32 +0500

    Add Spock Framework 2.4 for unit testing

---
+++ src/test/groovy/com/example/HelloWorldSpec.groovy
@@ -0,0 +1,37 @@
+package com.example
+
+import spock.lang.Specification
+
+class HelloWorldSpec extends Specification {
+
+    def "should print hello world message"() {
+        given:
+        def output = new ByteArrayOutputStream()
+        System.setOut(new PrintStream(output))
+
+        when:
+        HelloWorld.main([] as String[])
+
+        then:
+        output.toString().contains("Hello, World!")
+
+        cleanup:
+        System.setOut(System.out)
+    }
+
+    def "addition works correctly"() {
+        expect:
+        1 + 1 == 2
+    }
+
+    def "string operations work"() {
+        given:
+        def text = "Hello, World!"
+
+        expect:
+        text.length() == 13
+        text.startsWith("Hello")
+        text.endsWith("World!")
+        text.contains("World")
+    }
+}
